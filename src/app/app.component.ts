de07b37414551190bf26ca544c2fe6d7cdb5d15d
O O <nkwadioo@gmail.com>
2023-08-12 02:40:07 +0200

    Init & navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <app-menu />
+  `
 })
 export class AppComponent {
   title = 'APF-Party';
---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  styleUrls: ['./menu.component.scss'],
+  template: `
+    <section>
+      <img src="/assets/images/logo.jpg" alt="" />
+      <nav>
+        <a *ngFor="let item of menuItems" (click)="onSelect(item.title)" [routerLink]="item.route"
+          [class.active]="isActive(item.route)">
+          {{item.title}}
+        </a>
+      </nav>
+    </section>
+  `,
+})
+export class MenuComponent {
+  @Output() menuSelected = new EventEmitter<string>();
+
+  menuItems = [
+    { title: 'Home', route: '/' },
+    { title: 'About Us', route: '/about-us ' },
+    { title: 'Structure', route: '/structure' },
+    { title: 'Become A Membership', route: '/membership' },
+    { title: 'Contact Us', route: '/contact-us' },
+    // add other menu items here
+  ];
+  
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+
+  onSelect(menuItem: string): void {
+    this.menuSelected.emit(menuItem);
+  }
+
+  isActive(route: string): boolean {
+    // Compare the current route with the route you're checking
+    return this.router.url === route;
+  }
+}
